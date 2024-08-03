c0d23a7eff1ff5ccea2f5a89c680f6ae055f5ee2
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-08-03 20:07:34 +0300

    final fix

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 let speech = new SpeechSynthesisUtterance()
-
 let voices = []
 let voiceSelect = document.querySelector('select')
+let textarea = document.querySelector('textarea')
+
+function saveText() {
+  localStorage.setItem('savedText', textarea.value)
+}
+function saveVoice() {
+  localStorage.setItem('savedVoice', voiceSelect.value)
+}
+
+function restoreText() {
+  const savedText = localStorage.getItem('savedText')
+  if (savedText) {
+    textarea.value = savedText
+  }
+}
+function restoreVoice() {
+  const savedVoice = localStorage.getItem('savedVoice')
+  if (savedVoice) {
+    voiceSelect.value = savedVoice
+    speech.voice = voices[savedVoice]
+  }
+}
+
 window.speechSynthesis.onvoiceschanged = () => {
   voices = window.speechSynthesis.getVoices()
   speech.voice = voices[0]
@@ -9,13 +31,19 @@ window.speechSynthesis.onvoiceschanged = () => {
   voices.forEach(
     (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
   )
+  restoreVoice()
 }
 
 voiceSelect.addEventListener('change', () => {
   speech.voice = voices[voiceSelect.value]
+  saveVoice()
 })
 
 document.querySelector('button').addEventListener('click', () => {
-  speech.text = document.querySelector('textarea').value
+  speech.text = textarea.value
   window.speechSynthesis.speak(speech)
+  saveText()
 })
+
+restoreText()
+textarea.addEventListener('input', saveText)
